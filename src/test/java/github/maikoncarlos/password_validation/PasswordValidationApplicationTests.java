bc37f64a0b88fbbcae9947f621857b8451ff4b92
Maikoncarlos <maikonsaturno@gmail.com>
2024-04-10 20:47:58 -0300

    struct from project validation password

---
+++ src/test/java/github/maikoncarlos/password_validation/PasswordValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package github.maikoncarlos.password_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
