f1520a6374a4e1d7a3b867d645876f5bf66b2928
Maikoncarlos <maikonsaturno@gmail.com>
2024-04-12 16:00:30 -0300

    inserindo as Anotações para validar conforme regras de negócio

--- src/main/java/github/maikoncarlos/password_validation/controller/dto/PasswordRequestDTO.java
+++ src/main/java/github/maikoncarlos/password_validation/controller/dto/PasswordRequestDTO.java
@@ -1,4 +1,14 @@
 package github.maikoncarlos.password_validation.controller.dto;
 
-public record PasswordRequestDTO (String password){
+import jakarta.validation.constraints.*;
+
+public record PasswordRequestDTO(
+
+        @NotEmpty(message = "A Senha não pode ser vazia ou nula!")
+        @Size(min = 8, max = 15, message = "A Senha deve conter no mínimo 8 e no máximo 15 caracteres!")
+        @Pattern(regexp = ".*\\d.*", message = "A Senha deve conter pelo menos um número!")
+        @Pattern(regexp = ".*[A-Z].*", message = "A Senha deve conter pelo menos uma letra maiúscula!")
+        @Pattern(regexp = ".*[a-z].*", message = "A Senha deve conter pelo menos uma letra  minúscula!")
+        @Pattern(regexp = ".*\\W.*", message = "A Senha deve conter pelo menos um caracter especial!")
+        String password) {
 }
