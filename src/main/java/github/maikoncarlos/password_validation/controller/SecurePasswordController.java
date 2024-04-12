aa1e9c366149e4c257272a2c445ae69ddf354a2c
Maikoncarlos <maikonsaturno@gmail.com>
2024-04-11 23:23:15 -0300

    inserindo a dependencia da jetbrains para mapear que não devemos retornar nulos

--- src/main/java/github/maikoncarlos/password_validation/controller/SecurePasswordController.java
+++ src/main/java/github/maikoncarlos/password_validation/controller/SecurePasswordController.java
@@ -3,6 +3,7 @@ package github.maikoncarlos.password_validation.controller;
 import github.maikoncarlos.password_validation.controller.dto.PasswordRequestDTO;
 import github.maikoncarlos.password_validation.service.SecurePasswordService;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +16,9 @@ public class SecurePasswordController {
     private final SecurePasswordService passwordService;
 
     @PostMapping(value = "/validate-password")
-    public ResponseEntity<PasswordResponseDTO> validatePassword(@RequestBody PasswordRequestDTO requestDTO) {
+    public ResponseEntity<PasswordResponseDTO> validatePassword(@RequestBody @NotNull PasswordRequestDTO requestDTO) {
         var errorList = passwordService.validadePassword(requestDTO.password());
+
         if (errorList.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
