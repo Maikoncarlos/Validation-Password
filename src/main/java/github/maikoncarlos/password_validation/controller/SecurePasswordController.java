8d316e02a0b906d8fc9df75990d6e26db6eacd4f
Maikoncarlos <maikonsaturno@gmail.com>
2024-04-11 22:14:52 -0300

    criando classe Controller para entrada da requisição POST e tratamento dos retornos 204 ou 400

---
+++ src/main/java/github/maikoncarlos/password_validation/controller/SecurePasswordController.java
@@ -0,0 +1,25 @@
+package github.maikoncarlos.password_validation.controller;
+
+import github.maikoncarlos.password_validation.controller.dto.PasswordRequestDTO;
+import github.maikoncarlos.password_validation.service.SecurePasswordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SecurePasswordController {
+
+    private final SecurePasswordService passwordService;
+
+    @PostMapping(value = "/validate-password")
+    public ResponseEntity<PasswordResponseDTO> validatePassword(@RequestBody PasswordRequestDTO requestDTO) {
+        var errorList = passwordService.validadePassword(requestDTO.password());
+        if (errorList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.badRequest().body(new PasswordResponseDTO(errorList));
+    }
+}
